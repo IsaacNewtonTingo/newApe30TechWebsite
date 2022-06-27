c5fccf2dacbf0f908f66a8d8b2f441675989637b
Isaac Newton Tingo <newtone480@gmail.com>
2022-06-27 12:25:14 +0300

    Edit settings

--- Ape30TechnologiesProject/settings.py
+++ Ape30TechnologiesProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41nwei7sz%dd@-lfyele6g)75yyfo-+0&t1o1nrp=63d*h8(kf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
